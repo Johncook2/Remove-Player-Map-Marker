4e0917fbc2cdf28da2025c18c442ced7e8a02708
Johncook2 <126199075+Johncook2@users.noreply.github.com>
2024-08-21 17:18:51 +0200

    Made the mod
    Did it

---
+++ RemovePlayerMapMarker/RemovePlayerMapMarker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Winch.Core;
+using Harmonylib;
+
+namespace RemovePlayerMapMarker
+{
+	public class RemovePlayerMapMarker : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(RemovePlayerMapMarker)} has loaded!");
+		}
+	}
+
+	[HarmonyPatch(typeof(MapWindow), "GetPlayerPositionAsMapPosition")]
+	public static class MapWindow_GetPlayerPositionAsMapPosition_Patch
+	{
+		static void Postfix( ref Vector2 __result)
+		{
+			__result.Set(0,0);
+		}
+	}
+}
+
