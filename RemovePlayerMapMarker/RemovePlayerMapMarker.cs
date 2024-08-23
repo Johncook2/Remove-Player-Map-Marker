53890be55004d7cd151b699fc084de60516cf974
Johncook2 <126199075+Johncook2@users.noreply.github.com>
2024-08-23 18:08:00 +0200

    Removed Vector2
    Trying stuff to get the game to not blackscreen

--- RemovePlayerMapMarker/RemovePlayerMapMarker.cs
+++ RemovePlayerMapMarker/RemovePlayerMapMarker.cs
@@ -8,7 +8,7 @@ namespace RemovePlayerMapMarker
 	[HarmonyPatch(typeof(MapWindow), "GetPlayerPositionAsMapPosition")]
 	public static class MapWindow_GetPlayerPositionAsMapPosition_Patch
 	{
-		static void Postfix( ref Vector2 __result)
+		static void Postfix( ref __result)
 		{
 			__result.Set(0,0);
 		}
