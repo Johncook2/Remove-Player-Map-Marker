530b69633093a647e8a20f705e0503f7ee79073f
Johncook2 <126199075+Johncook2@users.noreply.github.com>
2024-08-28 10:27:10 +0200

    Create Loader.cs

---
+++ RemovePlayerMapMarker/Loader.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace Remove-Player-Map-Marker
+{
+    public class Loader
+    {
+        /// <summary>
+        /// This method is run by Winch to initialize your mod
+        /// </summary>
+        public static void Initialize()
+        {
+            new Harmony("com.Johncook.RemovePlayerMapMarker").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
